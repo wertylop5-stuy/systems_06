6ebf317bdc1db2305890e609260101ca211aff73
Stanley Lin <slin55055@gmail.com>
2017-10-13 15:36:07 -0400

    work

--- list.c
+++ list.c
@@ -1,7 +1,21 @@
-void print_list(struct node *) {
+#include<stdio.h>
+#include<stdlib.h>
+#include"include/list.h"
+
+void print_list(struct node *head) {
+	while (head) {
+		printf("%d\n", (head++)->data);
+	}
+}
+
+struct node* insert_front(struct node *head, int data) {
+	struct node *new = (struct node *)malloc(sizeof(struct node));
+	new->data = data;
+	new->next = head;
+	
+	return new;
+}
+
+struct node* free_list(struct node *head) {
 	
 }
-struct node* insert_front(struct node *, int) {
-	
-}
-struct node* free_list(struct node *);
--- main.c
+++ main.c
@@ -1,8 +1,85 @@
 #include<stdio.h>
-#include"include/main.h"
+#include"include/list.h"
 
 int main() {
+	/*
+	struct node hello;
+	hello.data = 3;
+	hello.next = NULL;
 	
+	print_list(&hello);
+	*/
+	
+	struct node *hi;
+	print_list(hi);
+	
+	hi = insert_front(hi, 10);
+	print_list(hi);
 	
 	return 0;
-}
+B:w
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+nating condition loopm
